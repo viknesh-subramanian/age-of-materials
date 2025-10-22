15e8dcc5f8772aff60ad1bf7ecaaf70930872a95
Viknesh Subramanian <viknesh@navakshinlabs.org>
2025-10-22 20:41:33 +0530

    Base currency changed from dollars to rupees

--- app/page.tsx
+++ app/page.tsx
@@ -171,7 +171,7 @@ export default function Home() {
                       {purchase.name}
                     </td>
                     <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 dark:text-gray-300">
-                      ${purchase.amount.toFixed(2)}
+                      ₹{purchase.amount.toFixed(2)}
                     </td>
                     <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 dark:text-gray-300">
                       {new Date(purchase.dateOfPurchase).toLocaleDateString()}
